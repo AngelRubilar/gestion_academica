ee7fbb062527122679c98a898082769dbcb5c8a4
AngelRubilar <angelrubilar07@gmail.com>
2026-05-14 17:25:11 -0400

    feat(api): barrel exports en common/

---
+++ apps/api/src/common/index.ts
@@ -0,0 +1,7 @@
+export * from './decorators/current-user.decorator';
+export * from './decorators/roles.decorator';
+export * from './filters/http-exception.filter';
+export * from './guards/roles.guard';
+export * from './interceptors/logging.interceptor';
+export * from './interceptors/transform.interceptor';
+export * from './types/request-user';
